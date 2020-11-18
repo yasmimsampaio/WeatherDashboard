152995a0389d84e9f670150cdf4a0e6f7ab3731c
Yasmim Sampaio <yasmimsampaio10@hotmail.com>
2020-11-18 15:53:16 -0500

    added the weather API and did bootstrap styling

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ $(document).ready(function(){
     function searchBtn(){
         var cities = $("#searchInput").val();
         console.log(cities);
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ cities + "&appid=7abf01c86fc755fc7a3d5b5221927bc7"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+            .then(function(response) {
+                console.log(response);
+                $("#cityTitle").text(response.name);
+            })
+        
     }
     $("#searchBtn").on("click", searchBtn);
 })
+
